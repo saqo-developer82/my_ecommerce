9446703cd8edb5d13dd49b9777c0de7d36dc45ec
Sargis <sargis.torosyan@esterox.am>
2024-07-14 12:41:14 +0400

    creating products resource

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Group::make()->schema([
+                    Section::make('Product Information')->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255)
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(fn (string $operation, string $state, Set $set) => $set('slug', str($state)->slug()->toString())),
+                        Forms\Components\TextInput::make('slug')
+                            ->required()
+                            ->disabled()
+                            ->maxLength(255)
+                            ->dehydrated()
+                            ->unique(Product::class, 'slug', ignoreRecord: true),
+                        Forms\Components\MarkdownEditor::make('description')
+                            ->columnSpanFull()
+                            ->fileAttachmentsDirectory('products')
+                    ])->columns(2),
+
+                    Section::make('Images')->schema([
+                        Forms\Components\FileUpload::make('images')
+                            ->multiple()
+                            ->directory('products')
+                            ->maxFiles(5)
+                            ->reorderable()
+                    ])
+                ])->columnSpan(2),
+
+                Group::make()->schema([
+                    Section::make('Price')->schema([
+                        Forms\Components\TextInput::make('price')
+                            ->numeric()
+                            ->required()
+                            ->prefix('USD'),
+                    ]),
+
+                    Section::make('Associations')->schema([
+                        Select::make('category_id') // TODO: error when creating a new product. Check and fix
+                            ->required()
+                            ->searchable()
+                            ->preload()
+                            ->relationship('category', 'name'),
+
+                        Select::make('brand_id')
+                            ->required()
+                            ->searchable()
+                            ->preload()
+                            ->relationship('brand', 'name'),
+                    ]),
+
+                    Section::make('Status')->schema([
+                        Forms\Components\Toggle::make('is_stock')
+                            ->required()
+                            ->default(true),
+
+                        Forms\Components\Toggle::make('is_active')
+                            ->required()
+                            ->default(true),
+
+                        Forms\Components\Toggle::make('is_featured')
+                            ->required(),
+
+                        Forms\Components\Toggle::make('on_sale')
+                            ->required(),
+                    ])
+                ])->columnSpan(1),
+            ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
